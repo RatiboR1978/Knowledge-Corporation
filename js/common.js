741756503bf65df1d73b2d6b80fa9c73b65d4e9a
Ratibor <klov29@mail.ru>
2018-06-22 17:49:25 +0700

    Адаптировал до мобилы

--- js/common.js
+++ js/common.js
@@ -1,8 +1,21 @@
 $(function () {
 
+    $('.description__slider').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 1
+    });
 
+    $("#menu1").on('click',function(){
+        var elem = $(this);
 
-
+        elem.toggleClass('menu_btn__active');
+        if(elem.hasClass('menu_btn__active')) {
+            $('.header__menu-mob').slideDown(500);
+        } else {
+            $('.header__menu-mob').slideUp(500);
+        }
+    });
 
 
 });
