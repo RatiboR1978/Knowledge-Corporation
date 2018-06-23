15edbfa604efda991ed03bc2b91a1e8e674b62c4
Ratibor <klov29@mail.ru>
2018-06-23 13:50:48 +0700

    Ступор с таблицей

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
 $(function () {
 
     $('.description__slider').slick({
-        dots: true,
         speed: 300,
-        slidesToShow: 1
+        slidesToShow: 1,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 810,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 
     $("#menu1").on('click',function(){
