56b3ea7c6cfe1688d49767c25e2c03d56c1de5f7
Ratibor <klov29@mail.ru>
2018-06-24 08:09:05 +0700

    Сделал все ховеры

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    /* Slick.js http://kenwheeler.github.io/slick/
+    ========================*/
+
     $('.description__slider').slick({
         speed: 300,
         slidesToShow: 1,
@@ -16,7 +19,10 @@ $(function () {
         ]
     });
 
-    $("#menu1").on('click',function(){
+    /* Menu-mobile
+    ========================*/
+
+    $('#menu1').on('click',function(){
         var elem = $(this);
 
         elem.toggleClass('menu_btn__active');
@@ -26,6 +32,22 @@ $(function () {
             $('.header__menu-mob').slideUp(500);
         }
     });
+
+    /* Tabs
+    ========================*/
+
+    $('.requests__tab-nav').on('click', function() {
+        alert(123)
+        $('.requests__tab-nav').removeClass('requests__tab-nav--active');
+        $('.requests__table-wrap').removeClass('requests__active');
+        $(this).addClass('requests__tab-nav--active');
+
+        if ($(this).html() === $('.requests__tab-nav:first').html()) {
+            $('.requests__table-wrap:first').addClass('requests__active');
+        } else {
+            $('.requests__table-wrap:last').addClass('requests__active');
+        }
+    })
 
 
 });
